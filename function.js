ad58d02e1cafdc855ad4fa975c1568c1f7380a01
angelagarciasempio <127348853+angelagarciasempio@users.noreply.github.com>
2023-03-08 18:05:44 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+function mostrar1(){
+  document.getElementById('Abrir1').style.display = 'block';}
+function ocultar1(){
+  document.getElementById('Abrir1').style.display = 'none';}
+function mostrar2(){
+  document.getElementById('Abrir2').style.display = 'block';}
+function ocultar2(){
+  document.getElementById('Abrir2').style.display = 'none';}
+  function mostrar3(){
+  document.getElementById('Abrir3').style.display = 'block';}
+  function ocultar3(){
+  document.getElementById('Abrir3').style.display = 'none';}
